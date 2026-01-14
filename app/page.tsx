c9ce460de8de8a8e94f5e2771d08b6f80ee9795e
위영민 <youngminieo1005@gmail.com>
2026-01-15 00:48:18 +0900

    chore: tailwind prefix 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,27 @@
 export default function Home() {
   return (
     <>
-     <p className="he:display-28-bd">Hello World</p>
-     <p className="he:heading-20-bd">Hello World</p>
-     <p className="he:heading-20-sb">Hello World</p>
-     <p className="he:heading-20-md">Hello World</p>
-     <p className="he:heading-20-rg">Hello World</p>
-     <p className="he:body-18-bd">Hello World</p>
-     <p className="he:body-18-sb">Hello World</p>
-     <p className="he:body-18-md">Hello World</p>
-     <p className="he:body-18-rg">Hello World</p>
-     <p className="he:body-16-bd">Hello World</p>
-     <p className="he:body-16-sb">Hello World</p>
-     <p className="he:body-16-md">Hello World</p>
-     <p className="he:body-16-rg">Hello World</p>
-     <p className="he:body-14-bd">Hello World</p>
-     <p className="he:body-14-sb">Hello World</p>
-     <p className="he:body-14-md">Hello World</p>
-     <p className="he:body-14-rg">Hello World</p>
-     <p className="he:caption-12-bd">Hello World</p>
-     <p className="he:caption-12-sb">Hello World</p>
-     <p className="he:caption-12-md">Hello World</p>
-     <p className="he:caption-12-rg">Hello World</p>
+     <p className="ygi:display-28-bd">Hello World</p>
+     <p className="ygi:heading-20-bd">Hello World</p>
+     <p className="ygi:heading-20-sb">Hello World</p>
+     <p className="ygi:heading-20-md">Hello World</p>
+     <p className="ygi:heading-20-rg">Hello World</p>
+     <p className="ygi:body-18-bd">Hello World</p>
+     <p className="ygi:body-18-sb">Hello World</p>
+     <p className="ygi:body-18-md">Hello World</p>
+     <p className="ygi:body-18-rg">Hello World</p>
+     <p className="ygi:body-16-bd">Hello World</p>
+     <p className="ygi:body-16-sb">Hello World</p>
+     <p className="ygi:body-16-md">Hello World</p>
+     <p className="ygi:body-16-rg">Hello World</p>
+     <p className="ygi:body-14-bd">Hello World</p>
+     <p className="ygi:body-14-sb">Hello World</p>
+     <p className="ygi:body-14-md">Hello World</p>
+     <p className="ygi:body-14-rg">Hello World</p>
+     <p className="ygi:caption-12-bd">Hello World</p>
+     <p className="ygi:caption-12-sb">Hello World</p>
+     <p className="ygi:caption-12-md">Hello World</p>
+     <p className="ygi:caption-12-rg">Hello World</p>
     </>
   );
 }
