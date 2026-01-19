d6309d0ae71f9779f5a5a96e0f51c4c87a7a0a80
위영민 <youngminieo1005@gmail.com>
2026-01-19 23:54:51 +0900

    feat: InputField 컴포넌트 추가

---
+++ src/components/inputField/InputField.tsx
@@ -0,0 +1,26 @@
+import { twJoin } from 'tailwind-merge';
+
+import { HelperText, type HelperTextProps } from './HelperText';
+import { Input, type InputProps } from './Input';
+
+export type InputFieldProps = InputProps &
+	HelperTextProps & {
+		containerClassName?: string;
+	};
+
+export const InputField = ({
+	helperText,
+	errorText,
+	containerClassName,
+	isError,
+	...inputProps
+}: InputFieldProps) => {
+	const hasError = isError || !!errorText;
+
+	return (
+		<div className={twJoin('ygi:flex ygi:flex-col ygi:gap-xs', containerClassName)}>
+			<Input {...inputProps} isError={hasError} />
+			<HelperText helperText={helperText} errorText={errorText} />
+		</div>
+	);
+};
