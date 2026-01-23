f79d9a1f2d4a9a624aee03f9bf29b6d5d3b8b6e4
위영민 <youngminieo1005@gmail.com>
2026-01-24 01:08:30 +0900

    chore: formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<div className="ygi:flex ygi:flex-col ygi:items-center ygi:justify-center ygi:h-screen">
+		<div className="ygi:flex ygi:h-screen ygi:flex-col ygi:items-center ygi:justify-center">
 			<h1>Hello World</h1>
 		</div>
 	);
